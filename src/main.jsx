1877b31608cb37fbc5032f403d667ba6f4b76b27
Milton <gmraivan@gmail.com>
2023-09-01 15:35:04 -0500

    Primerios pasos en react

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import {HelloWorldApp} from "./HelloWorldApp"
+import {FirstApp} from "./FirstApp"
+ReactDom.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FirstApp />
+  </React.StrictMode>
+
+)
