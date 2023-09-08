4d1dcd329da72bc08f7fa1d4e7fceeb189dd040d
milton cañari <devmeams>
2023-09-07 21:14:33 -0500

    primer fist

--- tests/FirstApp.test.js
+++ tests/FirstApp.test.js
@@ -3,8 +3,12 @@ import {FirstApp} from '../src/FirstApp'
 describe("Pruebas en FirstApp", () => {
   test("Debe hacer macth con el snapshot", () => {
       const title = "Myriam"
-      const {container}= render(<FirstApp title={title} />)
-       expect(container).toMatchSnapshot();
+      const {container,getByText}= render(<FirstApp title={title} />)
+    //   expect(container).toMatchSnapshot();
+    expect (getByText(title)).toBeTruthy();
+    const h1 =container.querySelector('h1')
+    console.log(h1.innerHTML)
+    expect(h1.innerHTML).toBe(title)
 
   });
 });
