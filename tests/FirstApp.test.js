64a0365118f3f433355e1a2c2ee1b15fcb2c854a
Milton <gmraivan@gmail.com>
2023-09-07 19:44:35 -0500

    tstrs

--- tests/FirstApp.test.js
+++ tests/FirstApp.test.js
@@ -2,8 +2,9 @@ import { render } from "@testing-library/react";
 import {FirstApp} from '../src/FirstApp'
 describe("Pruebas en FirstApp", () => {
   test("Debe hacer macth con el snapshot", () => {
-
-      render(<FirstApp />)
+      const title = "Myriam"
+      const {container}= render(<FirstApp title={title} />)
+       expect(container).toMatchSnapshot();
 
   });
 });
